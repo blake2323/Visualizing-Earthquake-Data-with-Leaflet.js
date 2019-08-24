3a75b010cd7700ca16d39bd95fe439d906834ca7
Blake Schwartzbach <blake2323@gmail.com>
2019-08-24 14:36:43 -0700

    added legend 0824236pm

--- static/js/logic.js
+++ static/js/logic.js
@@ -116,4 +116,39 @@ function createMap(earthquakes) {
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
+
+  
+  //Add legend with appropriate characteristics
+  var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+
+
+	var div = L.DomUtil.create('div', 'info legend'),
+		grades = [0, 1, 2, 3, 4],
+		labels = [];
+
+	// loop through our density intervals and generate a label with a colored square for each interval
+	for (var i = 0; i < grades.length; i++) {
+		div.innerHTML +=
+			'<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+			grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+	}
+
+	return div;
+};
+
+legend.addTo(myMap);
+
+}
+function getColor(d) {
+	return d > 4 ? 'red' :
+	       d > 3  ? 'orange' :
+	       d > 2  ? 'yellow' :
+	       d > 1  ? 'green' :
+	                  'blue';
+
+
+
 }
